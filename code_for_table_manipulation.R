39ed20501b6688a73f31331cbce34eb11dfb0629
HanShuliang <lijiajiazy@msn.cn>
2019-06-21 23:42:26 +0800

    1

---
+++ code_for_table_manipulation.R
@@ -0,0 +1,18 @@
+#批量合并csv文件的方式
+#1.using for loop
+getwd()
+list.files()
+files_full <- list.files(,full.names = TRUE)
+dat <- data.frame()
+for (i in 1:300) {
+  dat <- rbind(dat, read.csv(files_full[i]))
+  }
+
+#2.using map funtion
+library(tidyverse)
+
+data1<-list.files(path = "D:/R/test/",
+           pattern="*.csv", 
+           full.names = T) %>% 
+  map_df(~read_csv(.))
+
