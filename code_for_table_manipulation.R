b59c020e06d2f01d37347def9a6bcd4d8e479f93
HanShuliang <lijiajiazy@msn.cn>
2019-06-21 23:59:48 +0800

    1

--- code_for_table_manipulation.R
+++ code_for_table_manipulation.R
@@ -15,4 +15,24 @@ data1<-list.files(path = "D:/R/test/",
            pattern="*.csv", 
            full.names = T) %>% 
   map_df(~read_csv(.))
+#If there is a file that you want to exclude then
+list.files(path = "location_of/data/folder_you_want/",
+           pattern="*.csv", 
+           full.names = T) %>% 
+  .[ !grepl("data/folder/name_of_file_to_remove.csv",  .) ] %>%
+  map_df(~read_csv(.))
+
+#using apply
+setwd("D:/R/t/")
+filenames <- list.files(full.names=TRUE)
+All <- lapply(filenames,function(i){
+  read.csv(i, header=T)
+})
+df <- do.call(rbind.data.frame, All)
+
+
+
+
+
+
 
